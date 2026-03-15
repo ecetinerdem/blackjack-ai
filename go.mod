d16a4765bd85bbe6b390ceaf03957dc258db1ca8
ecetinerdem <cetoerdem1@gmail.com>
2026-03-15 13:23:41 +0400

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module blackjack-ai
+
+go 1.25.3
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+	// Clear the Screen
+
+	// Welcome message
+
+	// Get a deck of cards
+
+	// Create a card counter for the AI to use
+
+	for {
+		// Check to see if we need to shuffle the deck
+
+		// Play a round
+
+		// Ask if the player wants to play another round
+
+		// If not quit the game
+
+		// Clear the secreen again
+
+	}
+}
+
+// fmt.Print("\033[H\033[2J\033[3J")
