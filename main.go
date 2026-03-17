1d191bb1c1727640e9e6ce72087e926ecc8b122d
ecetinerdem <cetoerdem1@gmail.com>
2026-03-17 15:58:12 +0400

    playing round

--- main.go
+++ main.go
@@ -15,11 +15,19 @@ func main() {
 		fmt.Printf("%s%s\n", c.Value, c.Suit)
 	}
 	// Create a card counter for the AI to use
+	cardCounter := NewCardCounter()
+	fmt.Println(cardCounter.DecksRemaining)
 
 	for {
 		// Check to see if we need to shuffle the deck
-
+		if len(deck) < 10 {
+			fmt.Println("\n=== Decik is running low. Reshuffling... ===")
+			deck = NewDeck().Shuffle()
+			cardCounter.Reset()
+			fmt.Println("Deck reshuffled and card counter reset.")
+		}
 		// Play a round
+		PlayRound(&deck, cardCounter)
 
 		// Ask if the player wants to play another round
 
